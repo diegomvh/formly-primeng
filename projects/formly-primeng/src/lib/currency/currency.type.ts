f51938245658fed4f9ede6eb78a34979e327e285
Diego van Haaster <diegomvh@gmail.com>
2021-10-14 23:31:57 -0300

    More events

--- projects/formly-primeng/src/lib/currency/currency.type.ts
+++ projects/formly-primeng/src/lib/currency/currency.type.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  ViewChild,
-  OnDestroy,
-  AfterViewInit,
-  OnInit,
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { InputNumber } from 'primeng/inputnumber';
 import { PrimengComponentType } from '../prime.type';
 
@@ -16,6 +9,14 @@ import { PrimengComponentType } from '../prime.type';
       [mode]="'currency'"
       [currency]="to.currency || 'USD'"
       [locale]="to.locale || 'en-US'"
+      [currencyDisplay]="to.currencyDisplay || 'symbol'"
+      [suffix]="to.suffix || null"
+      [prefix]="to.prefix || null"
+      [min]="to.min || null"
+      [max]="to.min || null"
+      (onFocus)="to.onFocus && to.onFocus(field, $event)"
+      (onBlur)="to.onBlur && to.onBlur(field, $event)"
+      (onInput)="to.onInput && to.onInput(field, $event)"
       [formControl]="formControl"
       [formlyAttributes]="field"
     ></p-inputNumber>
--- projects/formly-primeng/src/lib/dropdown/dropdown.type.ts
+++ projects/formly-primeng/src/lib/dropdown/dropdown.type.ts
@@ -13,13 +13,21 @@ import { PrimengComponentType } from '../prime.type';
   selector: 'formly-primeng-dropdown',
   template: `
     <p-dropdown
+      appendTo="body"
       [placeholder]="to.placeholder"
       [options]="to.options | formlySelectOptions: field | async"
       [disabled]="to.disabled"
+      [showClear]="to.showClear || false"
+      (onClick)="to.onClick && to.onClick(field, $event)"
+      (onChange)="to.onChange && to.onChange(field, $event)"
+      (onFilter)="to.onFilter && to.onFilter(field, $event)"
+      (onFocus)="to.onFocus && to.onFocus(field, $event)"
+      (onBlur)="to.onBlur && to.onBlur(field, $event)"
+      (onShow)="to.onShow && to.onShow(field, $event)"
+      (onHide)="to.onHide && to.onHide(field, $event)"
+      (onClear)="to.onClear && to.onClear(field, $event)"
       [formControl]="formControl"
       [formlyAttributes]="field"
-      [showClear]="!to.required"
-      appendTo="body"
     >
     </p-dropdown>
   `,
