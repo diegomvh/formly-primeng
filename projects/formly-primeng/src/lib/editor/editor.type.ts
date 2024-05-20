a3de3d1decc6ac1632cec668cb312ddb1266d3d7
Diego van Haaster <diegomvh@gmail.com>
2024-05-20 09:56:52 -0300

    Filter treeSelect

--- projects/formly-primeng/src/lib/editor/editor.type.ts
+++ projects/formly-primeng/src/lib/editor/editor.type.ts
@@ -7,12 +7,12 @@ import { PrimengComponentType } from '../prime.type';
   template: `
     <p-editor
       [placeholder]="to.placeholder"
-      [style]="to.style || null"
-      [styleClass]="to.styleClass || null"
-      [readonly]="to.readonly || false"
-      [formats]="to.formats || null"
-      [modules]="to.modules || null"
-      [debug]="to.debug || null"
+      [style]="to.style ?? null"
+      [styleClass]="to.styleClass ?? null"
+      [readonly]="to.readonly ?? false"
+      [formats]="to.formats ?? null"
+      [modules]="to.modules ?? null"
+      [debug]="to.debug ?? null"
       (onTextChange)="to.onTextChange && to.onTextChange(field, $event)"
       (onSelectionChange)="
         to.onSelectionChange && to.onSelectionChange(field, $event)
